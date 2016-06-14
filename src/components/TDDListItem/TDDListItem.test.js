fae4e0070f9b05d85ebb3c1e08f56dfd04046fd0
Andre Giron <agiron123@gmail.com>
2016-06-13 22:07:03 -0400

    2. GREEN: ensure props can be passed into component.

--- src/components/TDDListItem/TDDListItem.test.js
+++ src/components/TDDListItem/TDDListItem.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import TDDListItem from './TDDListItem';
 
-describe('App', () => {
+import jsdom from 'jsdom';
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.document = doc;
+global.window = doc.defaultView;
+global.navigator = {userAgent: 'nodejs'};
+
+describe('TDDListItem', () => {
 
   it('renders TDDListItem', () => {
     const wrapper = shallow(
@@ -15,6 +21,35 @@ describe('App', () => {
     );
 
     expect(wrapper.contains(<div class="tddListItem"></div>)).to.equal(true);
+  });
+
+  it('allows us to pass in props', () => {
+    const wrapper = shallow(
+      <div>
+        <TDDListItem description="My Todo." completionStatus={false} />
+      </div>
+    );
+
+    const componentProps = wrapper.props().children.props;
+    expect(componentProps.description).to.equal("My Todo.");
+    expect(componentProps.completionStatus).to.equal(false);
+  });
+
+  it('contains a label and a checkbox', () => {
+    const wrapper = shallow(
+      <div>
+        <TDDListItem description="My Todo." completionStatus={false} />
+      </div>
+    );
+
+    //Check the rendered output for a checkbox.
+    const inputs = wrapper.find('input');
+    expect(inputs).to.have.length.of(1);
+
+    //Check the rendered output for a label
+    const labels = wrapper.find('label');
+    expect(labels).to.have.length.of(1);
+
   });
 
 });
