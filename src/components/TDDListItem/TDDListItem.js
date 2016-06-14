cb6802ea9ba1ce4b6f37f284285246cd7708b485
Andre Giron <agiron123@gmail.com>
2016-06-13 21:37:34 -0400

    Refactor TDDListItem component to be an ES6 class.

--- src/components/TDDListItem/TDDListItem.js
+++ src/components/TDDListItem/TDDListItem.js
@@ -1,14 +1,29 @@
 /**
  * Created by andre on 6/13/16.
  */
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './TDDListItem.css';
 
-function TDDListItem() {
-  return (
-    <div className={"tddListItem"}></div>
-  );
+//We need to design our component so that we actually know what to write a test for.
+//In our example, we want a checkbox to specify if the item is complete or not.
+//We also want a label to display the contents of the todo item.
+
+//Two important properties that we have for this component:
+//completionStatus, and description (the text for the TODO)
+//completionStatus and description will be changed by a parent components, so it will not be included in our state.
+
+class TDDListItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={"tddListItem"}>
+      </div>
+    );
+  }
 }
 
 export default withStyles(s)(TDDListItem);
